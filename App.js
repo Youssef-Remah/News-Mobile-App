d554fb386ec822546bf23e10d16d9c93e736e09f
Youssef-Remah <yremah8@gmail.com>
2023-09-18 13:52:34 +0300

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import MainApp from './components/MainApp';
+import { PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <PaperProvider>
+      
+      <MainApp/>
+
+    </PaperProvider>
+    
+
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/MainApp.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+function MainApp(props) {
+    return (
+        <View style={{ padding: 40 }}>
+            <Text>Hello From Main App</Text>
+
+        </View>
+    );
+}
+
+export default MainApp;
