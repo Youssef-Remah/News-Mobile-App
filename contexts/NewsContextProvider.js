5dcf94cf4449be1f319745520a36303613cebf3e
Youssef-Remah <yremah8@gmail.com>
2023-09-21 19:47:59 +0300

    Added Flat List

--- contexts/NewsContextProvider.js
+++ contexts/NewsContextProvider.js
@@ -1,51 +1,59 @@
 import { createContext, useEffect, useState } from 'react';
+import { Text, View } from "react-native";
 
 export const newsContext = createContext();
 
 //TODO: Hide the API Key!
-const URL = 'https://newsapi.org/v2/everything?q=tesla&from=2023-08-19&sortBy=publishedAt&apiKey=970bca636b994d51ab09cca3cdade332';
+const URL = 'https://newsapi.org/v2/everything?q=tesla&from=2023-08-21&sortBy=publishedAt&apiKey=970bca636b994d51ab09cca3cdade332';
 
-function NewsContextProvider({children}) {
+function NewsContextProvider({ children }) {
 
     const [news, setNews] = useState(null);
 
-    async function fetchData(){
+    async function fetchData() {
 
-        try
-        {
-            const response  = await fetch(URL);
-    
-            if(!response.ok)
-            {
-                throw new Error("Network response was not ok"); //TODO: Handle the response errors safely
+        try {
+            const response = await fetch(URL);
+
+            if (!response.ok) {
+                //throw new Error("Network response was not ok"); //TODO: Handle the response errors safely
+
+                <View>
+                    <Text>Network response was not ok</Text>
+                </View>
             }
-    
+
             const data = await response.json();
 
             return data;
 
         }
 
-        catch (error)
-        {
-            throw error; //TODO: Handle the response errors safely
+        catch (error) {
+            //throw error; //TODO: Handle the response errors safely
+
+            <View>
+                <Text>Error Encountered while loading the data...</Text>
+            </View>
 
         }
     };
 
-    useEffect(()=>{
+    useEffect(() => {
 
-        async function fetchNewsData(){
+        async function fetchNewsData() {
 
-            try
-            {
+            try {
                 const newsData = await fetchData();
 
                 setNews(newsData);
             }
-            catch (error)
-            {
-                throw error; //TODO: Handle the response errors safely
+            catch (error) {
+                //throw error; //TODO: Handle the response errors safely
+
+                <View>
+                    <Text>Error Encountered while loading the data...</Text>
+                </View>
             }
 
         }
@@ -55,7 +63,7 @@ function NewsContextProvider({children}) {
 
 
     return (
-        
+
         <newsContext.Provider value={news}>
 
             {children}
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,19 +1,55 @@
-import { Text, View } from "react-native";
+import { Text, View, ActivityIndicator, FlatList } from "react-native";
 import { useContext } from "react";
 import { newsContext } from "../contexts/NewsContextProvider";
+import uuid from 'react-native-uuid';
 
 function HomeScreen() {
 
     let news = useContext(newsContext);
 
-    news = news['articles'];
+    if(news == null || news == undefined)
+    {
+        return(
+            <View>
+                <ActivityIndicator size="large" />
 
+            </View>
+        )
+    }
+    else
+    {
+        
+        news = news['articles'];
+
+        news.forEach(
+            (article) => {
+
+                article.source.id = uuid.v4();
+            }
+        );
+    }
+
+    const renderItem =  ({item}) => {
+
+        return(
+            <View>
+                <Text>{item.source.name}</Text>
+            </View>
+        );
+
+    };
+    
     //console.warn(news);
 
     return (
         
         <View style={{ padding: 40 }}>
-            <Text>Hello From Home Screen</Text>
+
+            <FlatList
+                data={news}
+                keyExtractor={(item) => item.source.id}
+                renderItem={renderItem}
+            />
 
         </View>
 
